558a956b5b66444cb6fa95da5b6fa6d290a2851d
Jossue_Leon <jleon1934@utm.edu.ec>
2026-06-05 18:23:30 -0500

    Se implementa sistema Help Desk

--- HelpDeskSystem/src/model/TecnicoSoporte.java
+++ HelpDeskSystem/src/model/TecnicoSoporte.java
@@ -3,11 +3,9 @@ package model;
 public class TecnicoSoporte {
 
     private final String nombre;
-    private final String especialidad;
 
     public TecnicoSoporte(String nombre, String especialidad) {
         this.nombre = nombre;
-        this.especialidad = especialidad;
     }
 
     public void atenderTicket() {
